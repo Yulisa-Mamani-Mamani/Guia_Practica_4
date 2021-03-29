5b87266679f62089a846f62c3eb824db20f0de57
intel <intel@DESKTOP-2FB91R8>
2021-03-28 21:07:36 -0500

    SUBIDO 28-03-2021

---
+++ src/com/senati/eti/Caso_1.java
@@ -0,0 +1,20 @@
+package com.senati.eti;
+
+public class Caso_1 {
+
+	public static void main(String[] args) {
+		String[] nombre = new String[4];
+		
+		nombre[0] = "Joe Castillo";
+		nombre[1] = "Rosa Flores";
+		nombre[2] = "Ricardo Montes";
+		nombre[3] = "Viviana Figueroa";
+		
+		for (int i = 0; i < nombre.length; i++)
+			System.out.println("Nombre " + (i + 1) + ": " + nombre[i]);
+		
+			
+		
+	}
+
+}
